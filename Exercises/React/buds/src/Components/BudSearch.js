f6578c8c67e64c55ec3ea609a5d0953bfc2bcaf2
Damien Lowe <ashleydamienlowe@gmail.com>
2017-12-11 22:18:43 +1100

    budurl

--- Exercises/React/buds/src/Components/BudSearch.js
+++ Exercises/React/buds/src/Components/BudSearch.js
@@ -5,6 +5,21 @@ import Show from './Show';
 class BudSearch extends Component {
 
   //Define getInfo () AJAX REQUEST
+  getInfo (query) {
+    const budURL = "http://localhost:3000/";
+    jsonp(budUrl { callback: "jsoncallback" }).then( (results) => {
+      // console.log( results );
+      const images = results.photos.photo.map( generateURL );
+      // const images = [];
+      // for (let i = 0; i < results.photos.photo.lenth; i++) {
+      //   let image = generateURL( results.photos.photo[i] );
+      //   images.push(image);
+      // };
+
+      // console.log( images );
+      this.setState( { images } );
+    });
+  }
 
   render() {
     return (
