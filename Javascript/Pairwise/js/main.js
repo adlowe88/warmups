d9e60e36514215838a2d645081dd9f9722a48134
Damien Lowe <ashleydamienlowe@gmail.com>
2017-12-12 11:32:30 +1100

    pairwise option 2

--- Javascript/Pairwise/js/main.js
+++ Javascript/Pairwise/js/main.js
@@ -17,25 +17,41 @@
 
 //iterate through the array and see if i + ( i + 1 ) = num
 
+//
+// const pairwise = function (arr, num) {
+//   let indexSum = 0;
+//   //read input and sort from lowest to highest
+//   const sortedArray = arr.sort(function(a, b){return a - b});
+//
+//   for (let i = 0; i < sortedArray.length; i++) {
+//     //the value we are currently using as first index
+//     for (let j = i + 1; j < sortedArray.length; j++) {
+//       if (sortedArray[i] + sortedArray[j] === num) {
+//         indexSum = i + j;
+//         break;
+//       } else {
+//         continue;
+//       };
+//     };
+//   };
+//   return indexSum;
+// };
+//
+// console.log(pairwise([7, 9, 11, 13, 15], 20));
+// console.log(pairwise([2, 5, 1, -1, 2, 23], 7));
 
-const pairwise = function (arr, num) {
-  let indexSum = 0;
-  //read input and sort from lowest to highest
-  const sortedArray = arr.sort(function(a, b){return a - b});
-
-  for (let i = 0; i < sortedArray.length; i++) {
-    //the value we are currently using as first index
-    for (let j = i + 1; j < sortedArray.length; j++) {
-      if (sortedArray[i] + sortedArray[j] === num) {
-        indexSum = i + j;
-        break;
-      } else {
-        continue;
+const pairwise = function ( arr, arg ) {
+  const pairArr = arr.slice();
+  let sum = 0;
+  for (let i = 0; i < pairArr.length; i++) {
+    for (let j = i + 1; i < pairArr.length; j++) {
+      if ( pairArr[i] + pairArr[j] === arg) {
+        sum += i + j;
+        pairArr[i] = pairArr[j] = NaN;
       };
     };
   };
-  return indexSum;
+  return sum;
 };
 
 console.log(pairwise([7, 9, 11, 13, 15], 20));
-console.log(pairwise([2, 5, 1, -1, 2, 23], 7));
