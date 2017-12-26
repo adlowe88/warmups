c929244f2668a9f2e29362244abc23cf423699fc
Damien Lowe <ashleydamienlowe@gmail.com>
2017-12-27 10:19:56 +1100

    Ruby Atbash

---
+++ Ruby/atbash_cipher/main.rb
@@ -0,0 +1,25 @@
+# Atbash Cipher
+#
+# The Atbash cipher is a simple substitution cipher that relies on transposing all
+# the letters in the alphabet such that the resulting alphabet is backwards.
+# The first letter is replaced with the last letter, the second with the second-last, and so on.
+#
+# An Atbash cipher for the Latin alphabet would be as follows:
+#
+# Plain:  abcdefghijklmnopqrstuvwxyz
+# Cipher: zyxwvutsrqponmlkjihgfedcba
+# It is a very weak cipher because it only has one possible key, and it is a simple
+# monoalphabetic substitution cipher. However, this may not have been an issue in the cipher's time.
+#
+# Examples
+#
+# Encoding "test" gives "gvhg"
+# Decoding "gvhg" gives "test"
+
+class AtbashCipher
+
+  def initialize
+    @alphabet = ("a".."z").to_a
+    @cipherbet = ("a".."z").to_a.reverse 
+  end
+end
