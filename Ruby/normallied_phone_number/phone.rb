6a8dcf8fbbb3307e80d66e535a822b5dfa6ea3e4
Damien Lowe <ashleydamienlowe@gmail.com>
2018-01-11 10:22:39 +1100

    finished

--- Ruby/normallied_phone_number/phone.rb
+++ Ruby/normallied_phone_number/phone.rb
@@ -16,17 +16,37 @@ require 'pry'
 
 class Phone
   def initialize num
-
+    @num = num.gsub(/\W/, "")
+    get_matches
   end
 
-  def clean_number numb
-    binding.pry
-    #match any digit
-    @digits = str.match(/[0-9]/)
+  def validate
+    # @num.start_with("1")
+    if (@num.length == 11 && @num[0] == "1")
+      @num = @num[1..10]
+    elsif (@num.length != 10)
+      @num = "0000000000"
+    end
+    @num
   end
 
-  def remove_dots
-    if num.matc
+  def get_matches
+    @matches = @num.match(/(\d{3})(\d{3})(\d{4})/)
   end
+
+  def to_s
+    "(#{ @matches[1]}) #{ @matches[2] }-#{ @matches[3] }"
+  end
+
+  def area_code
+    @matches[1]
+  end
+
+  def number
+    validate
+    # binding.pry
+    # "1234567890"
+  end
+
 
 end
