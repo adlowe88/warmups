4a4aeafb13cd9125b3692206e4d1ae65a48cc9d1
Damien Lowe <ashleydamienlowe@gmail.com>
2018-01-14 10:30:57 +1100

    etch-a-sketch

--- Javascript/Etch-a-Sketch/js/main.js
+++ Javascript/Etch-a-Sketch/js/main.js
@@ -41,31 +41,52 @@ const draw = {
     x: 150,
     y: 150,
     size: 10,
+    hue: 0
   },
 
-  move: function (e) {
-    if ( e.key === "w" && this.pixel.y - this.pixel.size >= 0 ) {
-      this.pixel.y -= this.pixel.size;
-    } else if ( e.key === "a" && this.pixel.x - this.pixel.size >= 0 ) {
-      this.pixel.x -= this.pixel.size;
-    } else if ( e.key === "s" && this.pixel.y + this.pixel.size < $canvas.height ) {
-      this.pixel.y += thispixel.size;
-    } else if ( e) {
+  render: function(){
 
-    }
-  },
-
-  render: function () {
+    context.fillStyle = " hsla( " + this.pixel.hue + ", 100%, 50%, 1 ) "
 
     context.fillRect( draw.pixel.x, draw.pixel.y, draw.pixel.size, draw.pixel.size );
+
+    this.pixel.hue += 3;
+
   },
+
+  move: function( e ){
+    if( e.key === "w" && this.pixel.y - this.pixel.size >= 0 ){
+      this.pixel.y -= this.pixel.size;
+    } else if ( e.key === "a" && this.pixel.x - this.pixel.size >= 0 ){
+      this.pixel.x -= this.pixel.size;
+    } else if ( e.key === "s" && this.pixel.y + this.pixel.size < $canvas.height ){
+      this.pixel.y += this.pixel.size;
+    } else if ( e.key === "d" && this.pixel.x + this.pixel.size < $canvas.width ){
+      this.pixel.x += this.pixel.size;
+    } else {
+      return;
+    }
+    this.render();
+  }
+
 };
 
-window.onload = function () {
+window.onload = function(){
 
   $canvas = document.querySelector( 'canvas' );
 
   context = $canvas.getContext('2d');
 
   draw.render();
-}
+
+  document.querySelector('button').onclick = function(){
+
+    context.clearRect( 0, 0, $canvas.width, $canvas.height );
+
+  }
+
+};
+
+window.onkeypress = function( event ){
+  draw.move( event )
+};
